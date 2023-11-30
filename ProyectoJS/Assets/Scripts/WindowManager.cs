b229a617e1f8e862e2daf9f98cab06857e4e64e3
elisatodd <elitodd@ucm.es>
2023-11-30 15:47:54 +0100

    Nueva barra de herramientas

---
+++ ProyectoJS/Assets/Scripts/WindowManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private List<GameObject> myWindows;
+
+    [SerializeField]
+    private Button myButton;
+
+    public void AddWindow(GameObject window)
+    {
+        myWindows.Add(window);  
+    }
+
+    public void ToggleWindows()
+    {
+        foreach (var win in myWindows)
+        {
+            win.SetActive(!win.activeInHierarchy);
+        }
+    }
+
+    public void HideWindows()
+    {
+        foreach (var win in myWindows)
+        {
+            win.SetActive(false);
+        }
+    }
+
+
+}
